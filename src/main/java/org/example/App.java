76e272744688fb0f2a1cd9dbeaf5546544476f5c
Steven Dumortier <stevendumortier59@gmail.com>
2022-07-06 12:14:52 +0200

    Modif de pom, préparation pojo bean

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -7,7 +11,9 @@ package org.example;
 public class App 
 {
     public static void main( String[] args )
+
     {
         System.out.println( "Hello World!" );
+
     }
 }
