0ce228cc759b3a57f982d7e333ec91ff1493ceb3
Steven Dumortier <stevendumortier59@gmail.com>
2022-07-06 16:19:58 +0200

    ajout de findbyid et dontfind

--- src/main/java/org/example/dao/CustomerDAO.java
+++ src/main/java/org/example/dao/CustomerDAO.java
@@ -8,12 +8,19 @@ import javax.persistence.EntityTransaction;
 
 public class CustomerDAO {
 
-    public static void create(Customer customerToCreate){
+    public static void create(Customer customerToCreate) {
 
         EntityManager entityManager = EntityManagerSingleton.getEntityManager();
         EntityTransaction tx = entityManager.getTransaction();
         tx.begin();
         entityManager.persist(customerToCreate);
         tx.commit();
+    }
+
+    public static Customer findById(long id) {
+        EntityManager entityManager = EntityManagerSingleton.getEntityManager();
+        Customer customer = entityManager.find(Customer.class, id);
+        return customer;
+
     }
 }
